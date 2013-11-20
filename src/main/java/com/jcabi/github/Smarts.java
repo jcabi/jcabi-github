a2e895f5113194a6fdd5278330b25384b7b743f9
Yegor Bugayenko <yegor@tpc2.com>
2013-11-20 12:38:38 +0100

    Smarts

---
+++ src/main/java/com/jcabi/github/Smarts.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2012-2013, JCabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.github;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Smart items.
+ *
+ * <p>This class should be used as a decorator for an iterable of objects, for
+ * example:
+ *
+ * <pre> Iterable&lt;Issue.Smart&gt; issues = new Smarts&lt;Issue.Smart&gt;(
+ *   repo.issues().iterate(
+ *     new HashMap&lt;String, String&gt;()
+ *   )
+ * );
+ * for (Issue.Smart issue : issues) {
+ *     System.out.println("state is: " + issue.state());
+ * }</pre>
+ *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ * @since 0.5
+ * @param <T> Type of iterable objects
+ */
+@EqualsAndHashCode(of = "origin")
+public final class Smarts<T> implements Iterable<T> {
+
+    /**
+     * Original iterable.
+     */
+    private final transient Iterable<?> origin;
+
+    /**
+     * Public ctor.
+     * @param items Items original
+     */
+    public Smarts(final Iterable<?> items) {
+        this.origin = items;
+    }
+
+    @Override
+    public String toString() {
+        return this.origin.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        final Iterator<?> iterator = this.origin.iterator();
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+            @Override
+            public T next() {
+                return Smarts.<T>wrap(iterator.next());
+            }
+            @Override
+            public void remove() {
+                iterator.remove();
+            }
+        };
+    }
+
+    /**
+     * Wrap an object, and make a "smart" decorator.
+     * @param object Object to wrap
+     * @return Decorator
+     * @param <X> Type of result
+     */
+    @SuppressWarnings("unchecked")
+    private static <X> X wrap(final Object object) {
+        try {
+            return (X) Smarts.type(object).newInstance(object);
+        } catch (InvocationTargetException ex) {
+            throw new IllegalStateException(ex);
+        } catch (InstantiationException ex) {
+            throw new IllegalStateException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    /**
+     * Find "smart" class for the object.
+     * @param object Object to wrap
+     * @return Class to use
+     */
+    private static Constructor<?> type(final Object object) {
+        for (final Class<?> iface : object.getClass().getInterfaces()) {
+            try {
+                return Class.forName(
+                    String.format("%s$Smart", iface.getName())
+                ).getDeclaredConstructor(iface);
+            } catch (ClassNotFoundException ex) {
+                continue;
+            } catch (NoSuchMethodException ex) {
+                throw new IllegalStateException(ex);
+            }
+        }
+        throw new IllegalStateException(
+            String.format(
+                "can't find Smart decorator for %s",
+                object.getClass().getName()
+            )
+        );
+    }
+
+}
