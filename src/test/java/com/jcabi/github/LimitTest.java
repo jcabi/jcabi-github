0a73646034736ffc25d77fb3f45217882f6dd394
Giang Le <lthuangiang@gmail.com>
2013-12-20 13:39:02 +0700

    Issue #34 was implemented

---
+++ src/test/java/com/jcabi/github/LimitTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2012-2013, JCabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.github;
+
+import javax.json.Json;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Test case for {@link Limit}.
+ *
+ * @author Giang Le (giang@vn-smartsolutions.com)
+ * @version $Id$
+ */
+public final class LimitTest {
+
+    /**
+     * Describe Limit in a JSON object.
+     *
+     * @throws Exception ff there is any problem
+     */
+    @Test
+    public void json() throws Exception {
+        final Limit limit = Mockito.mock(Limit.class);
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("limit", "5")
+                .add("remaining", "3")
+                .add("reset", "123")
+                .build()
+        ).when(limit).json();
+        final Limit.Smart smart = new Limit.Smart(limit);
+        MatcherAssert.assertThat(
+            limit.json(),
+            Matchers.equalTo(smart.json())
+        );
+        MatcherAssert.assertThat(
+            limit.json().toString(),
+            Matchers.equalTo(
+                "{\"limit\":\"5\",\"remaining\":\"3\",\"reset\":\"123\"}"
+            )
+        );
+    }
+}
