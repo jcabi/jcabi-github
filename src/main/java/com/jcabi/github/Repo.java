4cf7174e90232c0ce560c981717e8319d9b4b1d4
Paul Polishchuk <ppol@ua.fake>
2013-12-25 00:10:14 +0200

    Issue #16 Todo was removed

--- src/main/java/com/jcabi/github/Repo.java
+++ src/main/java/com/jcabi/github/Repo.java
@@ -43,14 +43,6 @@ import lombok.ToString;
  * @author Yegor Bugayenko (yegor@tpc2.com)
  * @version $Id$
  * @since 0.1
- * @todo #1:1hr Assignees API should be implemented. Let's add a method
- *  assignees() to this class returning an instance of interface Assignees.
- *  This interface should have at least two methods: 1) iterate() returning
- *  a list of Users and 2) check(String) returning TRUE if provided
- *  login can be used as an assignee in repository. New interface should
- *  be implemented by GhAssignees class and tested in unit and integration
- *  tests. Moreover, we should implement MkAssignees class. See
- *  http://developer.github.com/v3/issues/assignees/
  */
 @Immutable
 @SuppressWarnings("PMD.TooManyMethods")
