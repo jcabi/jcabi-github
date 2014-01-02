649c22a66b1848d17c03374a79425e716d852e9d
Giang Le <lthuangiang@gmail.com>
2014-01-03 02:59:46 +0700

    Issue #104 - MkOrganizations.java: Mock for user's Organizations

--- src/main/java/com/jcabi/github/mock/MkOrganizations.java
+++ src/main/java/com/jcabi/github/mock/MkOrganizations.java
@@ -34,9 +34,15 @@ import com.jcabi.aspects.Loggable;
 import com.jcabi.github.Organization;
 import com.jcabi.github.Organizations;
 import com.jcabi.github.User;
+import com.jcabi.xml.XML;
+import java.io.IOException;
 import java.util.Map;
-import javax.validation.constraints.NotNull;
+import java.util.Random;
+import javax.json.Json;
+import javax.json.JsonObject;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.xembly.Directives;
 
 /**
  * Github organizations.
@@ -44,29 +50,119 @@ import lombok.ToString;
  * @version $Id$
  * @see <a href="http://developer.github.com/v3/orgs/">Organizations API</a>
  * @since 0.7
- * @todo #2 Mock for user's Organizations.
- *  Let's implements Mock for user's organizations using MkStorage.
- *  Don't forget about @EqualsAndHashCode.
  */
 @Immutable
 @Loggable(Loggable.DEBUG)
 @ToString
+@EqualsAndHashCode(of = { "storage", "self" })
 final class MkOrganizations implements Organizations {
+    /**
+     * Storage.
+     */
+    private final transient MkStorage storage;
+
+    /**
+     * Login of the user logged in.
+     */
+    private final transient String self;
+
+    /**
+     * Public ctor.
+     * @param stg Storage
+     * @param login User to login
+     * @throws IOException If there is any I/O problem
+     */
+    MkOrganizations(final MkStorage stg, final String login)
+        throws IOException {
+        this.storage = stg;
+        this.self = login;
+        this.storage.apply(
+            new Directives().xpath("/github").addIf("orgs")
+        );
+    }
 
     @Override
     public User user() {
-        return null;
+        try {
+            return new MkUser(this.storage, this.self);
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public Organization get(final int orgid) {
-        return null;
+        try {
+            this.storage.apply(
+                new Directives().xpath(this.xpath())
+                    .add("org")
+                    .add("id")
+                    .set(Integer.toString(orgid))
+            );
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+        // @checkstyle AnonInnerLength (50 lines)
+        return new Organization() {
+            @Override
+            public User user() {
+                return MkOrganizations.this.user();
+            }
+            @Override
+            public int orgId() {
+                return orgid;
+            }
+            @Override
+            public JsonObject json() {
+                return Json.createObjectBuilder()
+                    .add("name", "github")
+                    .add("company", "GitHub")
+                    .add("blog", "https://github.com/blog")
+                    .add("location", "San Francisco")
+                    .add("email", "octocat@github.com")
+                    .add("public_repos", new Random().nextInt())
+                    .add("public_gists", new Random().nextInt())
+                    .add("followers", new Random().nextInt())
+                    .add("following", new Random().nextInt())
+                    .add("html_url", "https://github.com/octocat")
+                    .add("created_at", "2008-01-14T04:33:35Z")
+                    .add("type", "Organization")
+                    .build();
+            }
+            @Override
+            public int compareTo(final Organization obj) {
+                return this.orgId() - obj.orgId();
+            }
+            @Override
+            public void patch(final JsonObject json) throws IOException {
+                new JsonPatch(MkOrganizations.this.storage)
+                    .patch(MkOrganizations.this.xpath(), json);
+            }
+        };
     }
 
     @Override
     public Iterable<Organization> iterate(
-        @NotNull(message = "map of params can't be NULL")
         final Map<String, String> params) {
-        return null;
+        return new MkIterable<Organization>(
+            this.storage,
+            String.format("%s/org", this.xpath()),
+            new MkIterable.Mapping<Organization>() {
+                @Override
+                public Organization map(final XML xml) {
+                    return MkOrganizations.this.get(
+                        Integer.parseInt(xml.xpath("id/text()").get(0))
+                    );
+                }
+            }
+        );
+    }
+
+    /**
+     * XPath of this element in XML tree.
+     * @return XPath
+     */
+    private String xpath() {
+        return "/github/orgs";
     }
 }
