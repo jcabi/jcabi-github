bf7ec11636e07affeffbfaaadb1b29e7bf239b6b
Alexei Popov <alopen@yandex.ru>
2014-01-04 10:34:09 +0700

    Redundant puzzle

--- src/main/java/com/jcabi/github/Gist.java
+++ src/main/java/com/jcabi/github/Gist.java
@@ -52,8 +52,6 @@ import lombok.ToString;
  *  http://developer.github.com/v3/gists/
  *  The method should be tested by integration and unit tests, and implemented
  *  in MkGist as well. When done, remove this comment.
- * @todo #19 Getting "Too many methods" error message. Does this class need
- *  refactoring?
  * @todo #1:1hr New method fork() to fork a gist. Let's introduce
  *  a new method, as explained in
  *  http://developer.github.com/v3/gists/#fork-a-gist. The method should
