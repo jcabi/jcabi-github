acce7aeb4d48c0b168faead2b2a4f39a1a36bab3
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2014-01-07 11:25:29 +0800

    Issue #124 Added RtSearch stub class, as well as unit and integration test classes.

---
+++ src/main/java/com/jcabi/github/RtSearch.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2012-2013, JCabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.github;
+
+import com.jcabi.aspects.Immutable;
+import com.jcabi.aspects.Loggable;
+import java.io.IOException;
+import javax.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Github Search.
+ *
+ * @author Carlos Miranda (miranda.cma@gmail.com)
+ * @version $Id$
+ * @since 0.8
+ * @todo #124 Add implementations of repos(), issues(), and users() methods.
+ *  When done, don't forget to remove @Ignore annotations from the
+ *  {@link RtSearchTest} class.
+ */
+@Immutable
+@Loggable(Loggable.DEBUG)
+@EqualsAndHashCode(of = { "ghub" })
+public final class RtSearch implements Search {
+
+    /**
+     * Github.
+     */
+    private final transient Github ghub;
+
+    /**
+     * Public ctor.
+     * @param github Github
+     */
+    public RtSearch(final Github github) {
+        this.ghub = github;
+    }
+
+    @Override
+    public Github github() {
+        return this.ghub;
+    }
+
+    @Override
+    public Iterable<Repo> repos(
+        @NotNull(message = "Search keywords can't be NULL")
+        final String keywords,
+        @NotNull(message = "Sort field can't be NULL") final String sort,
+        @NotNull(message = "Sort order can't be NULL") final String order)
+        throws IOException {
+        throw new UnsupportedOperationException(
+            "Repo search is not yet implemented."
+        );
+    }
+
+    @Override
+    public Iterable<Issue> issues(
+        @NotNull(message = "Search keywords can't be NULL")
+        final String keywords,
+        @NotNull(message = "Sort field can't be NULL") final String sort,
+        @NotNull(message = "Sort order can't be NULL") final String order)
+        throws IOException {
+        throw new UnsupportedOperationException(
+            "Issue search is not yet implemeted."
+        );
+    }
+
+    @Override
+    public Iterable<User> users(
+        @NotNull(message = "Search keywords can't be NULL")
+        final String keywords,
+        @NotNull(message = "Sort field can't be NULL") final String sort,
+        @NotNull(message = "Sort order can't be NULL") final String order)
+        throws IOException {
+        throw new UnsupportedOperationException(
+            "User search is not yet implemeted."
+        );
+    }
+
+}
