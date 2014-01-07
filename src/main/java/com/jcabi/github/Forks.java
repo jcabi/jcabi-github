3ab5b3f2c1d461ee159174fdd83c1b1c4cb7182f
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2014-01-07 14:23:01 +0800

    Issue #121 Created Fork and Forks interfaces, RtForks class stub, RtForksTest and RtForksITCase test classes.

---
+++ src/main/java/com/jcabi/github/Forks.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2012-2013, JCabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.github;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Github forks.
+ *
+ * @author Carlos Miranda (miranda.cma@gmail.com)
+ * @version $Id$
+ * @since 0.8
+ * @see <a href="http://developer.github.com/v3/repos/forks/">Forks API</a>
+ */
+public interface Forks {
+
+    /**
+     * Owner of them.
+     * @return Repo
+     */
+    @NotNull(message = "repository is never NULL")
+    Repo repo();
+
+    /**
+     * Iterate all forks.
+     *
+     * @param sort The sort order.
+     * @return All forks
+     * @see @see <a href="http://developer.github.com/v3/repos/forks/#list-forks">List forks</a>
+     */
+    @NotNull(message = "Iterable of forks is never NULL")
+    Iterable<Fork> iterate(@NotNull(message = "Sort order can't be NULL")
+        String sort);
+
+    /**
+     * Create a fork for the authenticated user.
+     *
+     * @param organization The organization the repository will be forked into.
+     * @return The new fork
+     * @see @see <a href="http://developer.github.com/v3/repos/forks/#create-a-fork">List forks</a>
+     */
+    Fork create(@NotNull(message = "map of params can't be NULL")
+        String organization);
+}
