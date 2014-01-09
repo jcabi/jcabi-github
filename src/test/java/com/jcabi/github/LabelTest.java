5a6ecc9b899e54928f459c91ef7c0b808036d073
Paul Polishchuk <ppol@ua.fake>
2014-01-09 23:55:17 +0200

    Unit test was added

---
+++ src/test/java/com/jcabi/github/LabelTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2012-2013, JCabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.github;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Tests for {@link Label}.
+ * @author Paul Polishchuk (ppol@ua.fm)
+ * @version $Id$
+ * @checkstyle MultipleStringLiterals (500 lines)
+ */
+public final class LabelTest {
+
+    /**
+     * Label.Unmodified can be compared properly.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void canBeComparedProperly() throws Exception {
+        final Label.Unmodified one =
+            new Label.Unmodified(
+                LabelTest.repo("jef", "jef_repo"),
+                "{\"name\":\"paul\"}");
+        final Label.Unmodified other =
+            new Label.Unmodified(
+                LabelTest.repo("stan", "stan_repo"),
+                "{\"name\":\"paul\"}");
+        Assert.assertFalse(one.equals(other));
+        Assert.assertFalse(one.compareTo(other) == 0);
+    }
+
+    /**
+     * Create and return repo for testing.
+     * @param user User name
+     * @param rpo Repo name
+     * @return Repo
+     */
+    private static Repo repo(final String user, final String rpo) {
+        final Repo repo = Mockito.mock(Repo.class);
+        Mockito.doReturn(new Coordinates.Simple(user, rpo))
+            .when(repo).coordinates();
+        return repo;
+    }
+}
