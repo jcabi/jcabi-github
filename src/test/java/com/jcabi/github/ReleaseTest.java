7a3eb781f33fd5b8aacc01b473952467ba3a23a4
denisanisimov <denis@vaadin.com>
2014-01-23 20:11:43 +0200

    Tests Release.Smart methods are updated.

--- src/test/java/com/jcabi/github/ReleaseTest.java
+++ src/test/java/com/jcabi/github/ReleaseTest.java
@@ -40,6 +40,7 @@ import org.mockito.Mockito;
  * @author Denis Anisimov (denis.nix.anisimov@gmail.com)
  * @version $Id$
  */
+@SuppressWarnings("PMD.TooManyMethods")
 public final class ReleaseTest {
 
     /**
@@ -50,15 +51,9 @@ public final class ReleaseTest {
     public void fetchesUrls() throws Exception {
         final Release release = Mockito.mock(Release.class);
         final String url = "http://url";
-        final String htmlurl = "http://html_url";
-        final String assetsurl = "http://assets_url";
-        final String uploadurl = "http://upload_url";
         Mockito.doReturn(
             Json.createObjectBuilder()
                 .add("url", url)
-                .add("html_url", htmlurl)
-                .add("assets_url", assetsurl)
-                .add("upload_url", uploadurl)
                 .build()
         ).when(release).json();
         final Release.Smart smart = new Release.Smart(release);
@@ -66,14 +61,62 @@ public final class ReleaseTest {
             smart.url().toString(),
             Matchers.equalTo(url)
         );
+    }
+
+    /**
+     * Release.Smart can fetch html url properties of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchesHtmlUrls() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String htmlurl = "http://html_url";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("html_url", htmlurl)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.htmlUrl().toString(),
             Matchers.equalTo(htmlurl)
         );
+    }
+
+    /**
+     * Release.Smart can fetch assets url properties of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchesAssetsHtmlUrls() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String assetsurl = "http://assets_url";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("assets_url", assetsurl)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.assetsUrl().toString(),
             Matchers.equalTo(assetsurl)
         );
+    }
+
+    /**
+     * Release.Smart can fetch upload url properties of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchesUploadHtmlUrls() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String uploadurl = "http://upload_url";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("upload_url", uploadurl)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.uploadUrl().toString(),
             Matchers.equalTo(uploadurl)
@@ -96,26 +139,16 @@ public final class ReleaseTest {
     }
 
     /**
-     * Release.Smart can fetch other properties properties of an Release.
+     * Release.Smart can fetch tag of an Release.
      * @throws Exception If some problem inside
      */
     @Test
-    public void fetchProperties() throws Exception {
+    public void fetchTag() throws Exception {
         final Release release = Mockito.mock(Release.class);
         final String tag = "v1.0.0";
-        final String master = "master";
-        final String name = "v1";
-        final String description = "Description of the release";
-        final String created = "2013-02-27T19:35:32Z";
-        final String published = "2013-01-27T19:35:32Z";
         Mockito.doReturn(
             Json.createObjectBuilder()
                 .add("tag_name", tag)
-                .add("target_commitish", master)
-                .add("name", name)
-                .add("body", description)
-                .add("created_at", created)
-                .add("published_at", published)
                 .build()
         ).when(release).json();
         final Release.Smart smart = new Release.Smart(release);
@@ -123,22 +156,102 @@ public final class ReleaseTest {
             smart.tag(),
             Matchers.equalTo(tag)
         );
+    }
+
+    /**
+     * Release.Smart can fetch commitish of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchProperties() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String master = "master";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("target_commitish", master)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.commitish(),
             Matchers.equalTo(master)
         );
+    }
+
+    /**
+     * Release.Smart can fetch name of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchName() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String name = "v1";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("name", name)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.name(),
             Matchers.equalTo(name)
         );
+    }
+
+    /**
+     * Release.Smart can fetch body of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchBody() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String description = "Description of the release";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("body", description)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.body(),
             Matchers.equalTo(description)
         );
+    }
+
+    /**
+     * Release.Smart can fetch created date of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchDescription() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String created = "2013-02-27T19:35:32Z";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("created_at", created)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.createdAt(),
             Matchers.equalTo(new Github.Time(created).date())
         );
+    }
+
+    /**
+     * Release.Smart can fetch published date of an Release.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void fetchPublished() throws Exception {
+        final Release release = Mockito.mock(Release.class);
+        final String published = "2013-01-27T19:35:32Z";
+        Mockito.doReturn(
+            Json.createObjectBuilder()
+                .add("published_at", published)
+                .build()
+        ).when(release).json();
+        final Release.Smart smart = new Release.Smart(release);
         MatcherAssert.assertThat(
             smart.publishedAt(),
             Matchers.equalTo(new Github.Time(published).date())
