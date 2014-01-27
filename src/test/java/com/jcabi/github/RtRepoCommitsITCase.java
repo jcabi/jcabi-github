1e3fb1bfd4df75ef2a893fff71529984a31dc8df
Alexander Cherednichenko <lexaux@gmail.com>
2014-01-27 16:13:37 +0200

    Issue #276 Test case for RtRepoCommits.

--- src/test/java/com/jcabi/github/RtRepoCommitsITCase.java
+++ src/test/java/com/jcabi/github/RtRepoCommitsITCase.java
@@ -29,15 +29,77 @@
  */
 package com.jcabi.github;
 
+import com.rexsl.test.Request;
+import com.rexsl.test.request.ApacheRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assume;
+import org.junit.Test;
+
 /**
  * Integration case for {@link RepoCommits}.
  * @author Alexander Sinyagin (sinyagin.alexander@gmail.com)
  * @version $Id$
- * @todo #117 Add test fetchCommits() to check that commits actually fetched.
- *  See
- *  http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository.
- * @todo #117 Add test getCommit() to check that commit actually got.
- *  See http://developer.github.com/v3/repos/commits/#get-a-single-commit.
  */
 public class RtRepoCommitsITCase {
+
+    public final transient String requestUrl = "https://api.github.com/";
+    public final transient String user = "jcabi";
+    public final transient String repo = "jcabi-github";
+
+    /**
+     * RtRepoCommits can fetch commits.
+     */
+    @Test
+    public void fetchCommits() {
+        final Request request = new ApacheRequest(requestUrl);
+        final Coordinates coordinates = new Coordinates.Simple(user, repo);
+
+        RepoCommits repoCommits = new RtRepoCommits(request, coordinates);
+        Iterator<Commit> iterator = repoCommits.iterate().iterator();
+
+        final List<String> shas = new ArrayList<String>();
+        shas.add("1aa4af45aa2c56421c3d911a0a06da513a7316a0");
+        shas.add("940dd5081fada0ead07762933036bf68a005cc40");
+        shas.add("05940dbeaa6124e4a87d9829fb2fce80b713dcbe");
+        shas.add("51cabb8e759852a6a40a7a2a76ef0afd4beef96d");
+        shas.add("11bd4d527236f9cb211bc6667df06fde075beded");
+        int existsCount = 0;
+
+        while (iterator.hasNext()) {
+            if (shas.contains(iterator.next().sha())) {
+                existsCount++;
+            }
+        }
+
+        MatcherAssert.assertThat(existsCount,
+            Matchers.equalTo(shas.size())
+        );
+    }
+
+    /**
+     * RtRepoCommits can get commit.
+     */
+    @Test
+    public void getCommit() throws IOException {
+        final Request request = new ApacheRequest(requestUrl);
+        final Coordinates coordinates = new Coordinates.Simple(user, repo);
+        final RepoCommits repoCommits = new RtRepoCommits(request, coordinates);
+
+        final String sha = "51cabb8e759852a6a40a7a2a76ef0afd4beef96d";
+        final String expectedName = "\"Alexander Sinyagin\"";
+        final Commit commit = repoCommits.get(sha);
+
+        MatcherAssert.assertThat(commit.json()
+            //.getJsonObject("commit")
+            .getJsonObject("author")
+            .get("name")
+            .toString(),
+            Matchers.equalTo(expectedName)
+        );
+    }
 }
