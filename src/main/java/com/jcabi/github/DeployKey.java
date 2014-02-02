78d5438ee826cf37ceea1948b213c100a0d6f48a
Alexei Popov <alopen@yandex.ru>
2014-02-02 21:02:00 +0700

    Left a puzzle

--- src/main/java/com/jcabi/github/DeployKey.java
+++ src/main/java/com/jcabi/github/DeployKey.java
@@ -49,6 +49,10 @@ import java.io.IOException;
  *  RtDeployKey and MkDeployKey, and add an integration test for it. See
  *  http://developer.github.com/v3/repos/keys/#delete. When done, remove this
  *  puzzle.
+ * @todo #356:1hr There should be get() method implemented for reading the value
+ *  of title and key. Implement method at RtDeployKey, MkDeployKey and write an
+ *  integration test for it. See http://developer.github.com/v3/repos/keys/#get
+ *  When done remove this puzzle and finish #356 (write test for edit method)
  */
 @Immutable
 public interface DeployKey extends JsonReadable {
