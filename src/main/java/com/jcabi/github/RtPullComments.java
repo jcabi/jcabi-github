77190e252d23bc98645839a00cc1addf07a71e3d
Joseph Arrieta <jac.wshmstr@gmail.com>
2014-02-18 10:13:32 -0430

    issue 472

--- src/main/java/com/jcabi/github/RtPullComments.java
+++ src/main/java/com/jcabi/github/RtPullComments.java
@@ -36,6 +36,7 @@ import com.jcabi.http.response.RestResponse;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.Map;
+import javax.json.JsonObject;
 import lombok.EqualsAndHashCode;
 
 /**
@@ -94,8 +95,18 @@ public final class RtPullComments implements PullComments {
 
     @Override
     public Iterable<PullComment> iterate(final Map<String, String> params) {
-        //@checkstyle MultipleStringLiteralsCheck (1 line)
-        throw new UnsupportedOperationException("Iterate not yet implemented.");
+        return new RtPagination<PullComment>(
+            this.request.uri().queryParams(params).back(),
+            new RtPagination.Mapping<PullComment, JsonObject>() {
+                @Override
+                public PullComment map(final JsonObject value) {
+                    return new RtPullComment(
+                        RtPullComments.this.request,
+                        RtPullComments.this.owner, value.getInt("id")
+                    );
+                }
+            }
+        );
     }
 
     @Override
