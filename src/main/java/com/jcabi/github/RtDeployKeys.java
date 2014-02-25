a94ea748ea9d0bdda2658953a06d41667adf114a
Giang Le <lthuangiang@gmail.com>
2014-02-25 15:40:06 +0700

    Issue #547 - Method RtDeployKeys#remove(int number) is redundant

--- src/main/java/com/jcabi/github/RtDeployKeys.java
+++ src/main/java/com/jcabi/github/RtDeployKeys.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.Collections;
 import javax.json.Json;
-import javax.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 
 /**
@@ -114,21 +113,5 @@ public final class RtDeployKeys implements DeployKeys {
                 .as(JsonResponse.class)
                 .json().readObject().getInt("id")
         );
-    }
-
-    /**
-     * Remove a deploy key by its id.
-     * @param number Id of the key to be remove.
-     * @throws IOException if something goes wrong.
-     */
-    public void remove(
-        @NotNull(message = "id can't be NULL") final int number)
-        throws IOException {
-        this.request.uri()
-            .path(Integer.toString(number))
-            .back()
-            .method(Request.DELETE)
-            .fetch().as(RestResponse.class)
-            .assertStatus(HttpURLConnection.HTTP_NO_CONTENT);
     }
 }
