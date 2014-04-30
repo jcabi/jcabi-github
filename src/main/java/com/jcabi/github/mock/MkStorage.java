12a791a623515e59542cc6cb2926ecb2fba3e3b9
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2014-04-30 09:34:18 +0800

    #720 Updated Javadoc as per review comment

--- src/main/java/com/jcabi/github/mock/MkStorage.java
+++ src/main/java/com/jcabi/github/mock/MkStorage.java
@@ -74,16 +74,28 @@ public interface MkStorage {
     ) throws IOException;
 
     /**
-     * Locks storage to the current thread. If the lock is available, grant it
+     * Locks storage to the current thread.
+     *
+     * <p>If the lock is available, grant it
      * to the calling thread and block all operations from other threads.
      * If not available, wait for the holder of the lock to release it with
      * {@link #unlock()} before any other operations can be performed.
+     *
+     * <p>Locking behavior is reentrant, which means a thread can invoke
+     * {@link #lock()} multiple times, where a hold count is maintained.
      * @throws IOException If there is any I/O problem
      */
     void lock() throws IOException;
 
     /**
-     * Unlock storage. This releases the lock, if held by the current thread.
+     * Unlock storage.
+     *
+     * <p>Locking behavior is reentrant, thus if the thread invoked
+     * {@link #lock()} multiple times, the hold count is decremented. If the
+     * hold count reaches 0, the lock is released.
+     *
+     * <p>If the current thread does not hold the lock, an
+     * {@link IllegalMonitorStateException} will be thrown.
      * @throws IOException If there is any I/O problem
      */
     void unlock() throws IOException;
