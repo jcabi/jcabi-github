b204afcbd5cab05ec51f1ed719d6adcf3ead83e1
Ed <edhillmann@yahoo.com>
2014-07-01 21:57:38 +1000

    Add tests, although they don't work yet and are ignored

--- src/main/java/com/jcabi/github/mock/MkGit.java
+++ src/main/java/com/jcabi/github/mock/MkGit.java
@@ -39,10 +39,16 @@ import com.jcabi.github.References;
 import com.jcabi.github.Repo;
 import com.jcabi.github.Tags;
 import com.jcabi.github.Trees;
+
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.validation.constraints.NotNull;
+
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
 import org.xembly.Directives;
 
 /**
@@ -113,7 +119,11 @@ final class MkGit implements Git {
     @Override
     @NotNull(message = "commits is never NULL")
     public Commits commits() {
-        throw new UnsupportedOperationException("Commits not yet implemented");
+        try {
+            return new MkCommits(this.storage, this.self, this.coords);
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
