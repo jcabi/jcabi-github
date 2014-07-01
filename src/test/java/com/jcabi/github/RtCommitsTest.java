4de45f8b86b08b2bbbeb534e28f2009b1cbaefad
Ed <edhillmann@yahoo.com>
2014-07-01 21:57:38 +1000

    Add tests, although they don't work yet and are ignored

---
+++ src/test/java/com/jcabi/github/RtCommitsTest.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2013-2014, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jcabi.github;
+
+import com.jcabi.github.mock.MkGithub;
+import com.jcabi.http.Request;
+import com.jcabi.http.mock.MkAnswer;
+import com.jcabi.http.mock.MkContainer;
+import com.jcabi.http.mock.MkGrizzlyContainer;
+import com.jcabi.http.request.ApacheRequest;
+
+import java.net.HttpURLConnection;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Testcase for RtCommits.
+ * @author Ed Hillmann (edhillmann@yahoo.com)
+ * @version $Id$
+ */
+public class RtCommitsTest {
+
+    @Test
+    public void createsCommit() throws Exception {
+        final MkContainer container = new MkGrizzlyContainer().next(
+            new MkAnswer.Simple(
+                HttpURLConnection.HTTP_CREATED,
+                "{\"sha\":\"0abcd89jcabitest\"}"
+            )
+        ).start();
+        final Commits commits = new RtCommits(
+            new ApacheRequest(container.home()),
+            repo()
+        );
+        final JsonObject author = Json.createObjectBuilder()
+            .add("name", "Scott").add("email", "scott@gmail.com")
+            .add("date", "2011-06-17T14:53:35-07:00").build();
+        final JsonObject input = Json.createObjectBuilder()
+            .add("message", "initial version")
+            .add("author", author).build();
+        try {
+            MatcherAssert.assertThat(
+                commits.create(input),
+                Matchers.instanceOf(Commit.class)
+            );
+            MatcherAssert.assertThat(
+                container.take().method(),
+                Matchers.equalTo(Request.POST)
+            );
+            MatcherAssert.assertThat(
+                container.take().method(),
+                Matchers.equalTo(Request.POST)
+            );
+        } finally {
+            container.stop();
+        }
+    }
+    
+    /**
+     * This method returns a Repo for testing.
+     * @return Repo - a repo to be used for test.
+     * @throws Exception - if anything goes wrong.
+     */
+    private static Repo repo() throws Exception {
+        return new MkGithub().repos().create(
+            Json.createObjectBuilder().add("name", "test").build()
+        );
+    }
+}
