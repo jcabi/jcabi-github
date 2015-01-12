0d5f5d80dfc626e7de828e98a75f96bc3beb98a6
Yegor Bugayenko <yegor@tpc2.com>
2015-01-12 21:19:32 +0100

    #980 RepoCommit.Smart.message() fixed

--- src/main/java/com/jcabi/github/RepoCommit.java
+++ src/main/java/com/jcabi/github/RepoCommit.java
@@ -103,7 +103,7 @@ public interface RepoCommit extends Comparable<RepoCommit>, JsonReadable {
          */
         @NotNull(message = "message is never NULL")
         public String message() throws IOException {
-            return this.jsn.text("message");
+            return this.jsn.json().getJsonObject("commit").getString("message");
         }
         /**
          * Get its URL.
