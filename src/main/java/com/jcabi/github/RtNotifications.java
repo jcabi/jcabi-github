87ecbb21630022065af95c0a6befad6a765cfd4e
Dmitri Pisarenko <dp@altruix.co>
2015-01-17 19:11:07 +0300

    Removed descriptions from overidden methods.

--- src/main/java/com/jcabi/github/RtNotifications.java
+++ src/main/java/com/jcabi/github/RtNotifications.java
@@ -31,6 +31,7 @@ package com.jcabi.github;
 
 import com.jcabi.aspects.Immutable;
 import java.util.Collections;
+import org.apache.commons.lang3.NotImplementedException;
 
 /**
  * Github Notifications API.
@@ -53,25 +54,13 @@ final class RtNotifications implements Notifications {
         return new RtNotification(number);
     }
 
-    /**
-     * Marks all notifications on this repository as read.
-     * @see <a href="https://developer.github.com/v3/activity/notifications/#mark-notifications-as-read-in-a-repository">Mark notifications as read in a repository</a>
-     * @todo #913:30min Implement method RtNotifications.markAsRead.
-     */
     @Override
     public void markAsRead() {
-        // Will be implemented later
+        throw new NotImplementedException("MkNotifications#markAsRead");
     }
 
-    /**
-     * Get thread data.
-     * @see <a href="https://developer.github.com/v3/activity/notifications/#view-a-single-thread">View a single thread</a>
-     * @param number Thread ID.
-     * @return Data of the specified thread.
-     * @todo #913:30min Implement method RtNotifications.thread.
-     */
     @Override
     public GitHubThread thread(final int number) {
-        return null;
+        throw new NotImplementedException("MkNotifications#thread");
     }
 }
