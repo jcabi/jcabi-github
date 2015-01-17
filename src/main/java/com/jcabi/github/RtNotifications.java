4ffa913d2dc301969defcf0e276fc5d3c4e58a9f
Dmitri Pisarenko <dp@altruix.co>
2015-01-17 16:56:41 +0300

    Fixed todo statements.

--- src/main/java/com/jcabi/github/RtNotifications.java
+++ src/main/java/com/jcabi/github/RtNotifications.java
@@ -56,7 +56,7 @@ final class RtNotifications implements Notifications {
     /**
      * Marks all notifications on this repository as read.
      * @see <a href="https://developer.github.com/v3/activity/notifications/#mark-notifications-as-read-in-a-repository">Mark notifications as read in a repository</a>
-     * @todo Implement this method
+     * @todo #913:30min Implement method RtNotifications.markAsRead.
      */
     @Override
     public void markAsRead() {
@@ -68,7 +68,7 @@ final class RtNotifications implements Notifications {
      * @see <a href="https://developer.github.com/v3/activity/notifications/#view-a-single-thread">View a single thread</a>
      * @param number Thread ID.
      * @return Data of the specified thread.
-     * @todo Implement this method
+     * @todo #913:30min Implement method RtNotifications.thread.
      */
     @Override
     public GitHubThread thread(final int number) {
