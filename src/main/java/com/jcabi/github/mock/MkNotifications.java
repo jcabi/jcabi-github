fab80e4b99b829055d4f14a020c8acf623aeaa3a
Dmitri Pisarenko <dp@altruix.co>
2015-01-17 18:48:55 +0300

    Removed unnecessary comments.

--- src/main/java/com/jcabi/github/mock/MkNotifications.java
+++ src/main/java/com/jcabi/github/mock/MkNotifications.java
@@ -58,23 +58,11 @@ final class MkNotifications implements Notifications {
         throw new NotImplementedException("MkNotifications#get");
     }
 
-    /**
-     * Marks all notifications on this repository as read.
-     * @see <a href="https://developer.github.com/v3/activity/notifications/#mark-notifications-as-read-in-a-repository">Mark notifications as read in a repository</a>
-     * @todo Implement this method
-     */
     @Override
     public void markAsRead() {
         // Will be implemented later
     }
 
-    /**
-     * Get thread data.
-     * @see <a href="https://developer.github.com/v3/activity/notifications/#view-a-single-thread">View a single thread</a>
-     * @param number Thread ID.
-     * @return Data of the specified thread.
-     * @todo Implement this method
-     */
     @Override
     public GitHubThread thread(final int number) {
         return null;
