e25c91628fdd4631506a491235214c2e1d50d506
cyberone <alopen@yandex.ru>
2015-01-29 07:15:14 +0600

    Wrong puzzle format

--- src/test/java/com/jcabi/github/RtForkTest.java
+++ src/test/java/com/jcabi/github/RtForkTest.java
@@ -53,7 +53,7 @@ import org.junit.Test;
 public final class RtForkTest {
     /**
      * The rule for skipping test if there's BindException.
-     * @todo 989 Apply this rule to other classes that use MkGrizzlyContainer
+     * @todo #989 Apply this rule to other classes that use MkGrizzlyContainer
      *  and make MkGrizzlyContainers use port() given by this resource to avoid
      *  tests fail with BindException.
      * @checkstyle VisibilityModifierCheck (3 lines)
