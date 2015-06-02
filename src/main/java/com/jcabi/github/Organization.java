d0523357e7cb2483c68b1f7fdb87105ef27e0b5d
Chris Rebert <code@rebertia.com>
2015-06-02 00:41:58 -0700

    Fix wrong @see links in JavaDocs for Organization & Users

--- src/main/java/com/jcabi/github/Organization.java
+++ src/main/java/com/jcabi/github/Organization.java
@@ -54,7 +54,7 @@ import lombok.ToString;
  * @author Paul Polishchuk (ppol@ua.fm)
  * @version $Id$
  * @checkstyle MultipleStringLiterals (500 lines)
- * @see <a href="http://developer.github.com/v3/issues/">Issues API</a>
+ * @see <a href="https://developer.github.com/v3/orgs/">Organizations API</a>
  * @since 0.7
  */
 @Immutable
