cbffe7565cb2cd9ec1de55bdd22f42378845b7aa
Chris Rebert <code@rebertia.com>
2015-06-17 01:49:39 -0700

    Update RtStatusesTest after Status API overhaul

--- src/test/java/com/jcabi/github/RtStatusesTest.java
+++ src/test/java/com/jcabi/github/RtStatusesTest.java
@@ -29,16 +29,20 @@
  */
 package com.jcabi.github;
 
+import com.google.common.base.Optional;
 import com.jcabi.github.mock.MkGithub;
 import com.jcabi.http.Request;
 import com.jcabi.http.mock.MkAnswer;
 import com.jcabi.http.mock.MkContainer;
 import com.jcabi.http.mock.MkGrizzlyContainer;
+import com.jcabi.http.mock.MkQuery;
 import com.jcabi.http.request.ApacheRequest;
 import com.jcabi.http.request.FakeRequest;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import javax.json.Json;
+import javax.json.JsonObject;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -53,7 +57,7 @@ import org.junit.Test;
  * @todo #1130:30min Write RtStatusesITCase, an integration test case for
  *  RtStatuses/RtStatus against real GitHub commit status data.
  */
-public class RtStatusesTest {
+public final class RtStatusesTest {
     /**
      * RtStatuses can fetch its commit.
      * @throws IOException If there is an I/O problem.
@@ -74,38 +78,64 @@ public class RtStatusesTest {
      * @throws Exception when an Error occurs
      */
     @Test
-    public final void createsStatus() throws Exception {
+    public void createsStatus() throws Exception {
+        final String stateprop = "state";
+        final String urlprop = "target_url";
+        final String descriptionprop = "description";
+        final String contextprop = "context";
+        final String url = "https://ci.example.com/1000/output";
+        final String description = "Build has completed successfully";
+        final String context = "continuous-integration/jenkins";
         final MkContainer container = new MkGrizzlyContainer().next(
             new MkAnswer.Simple(
                 HttpURLConnection.HTTP_CREATED,
-                Json.createObjectBuilder().add("state", "failure")
-                    .add("target_url", "https://ci.example.com/1000/output")
-                    .add("description", "Build has completed successfully")
-                    .add("context", "continuous-integration/jenkins")
+                Json.createObjectBuilder().add(stateprop, "failure")
+                    .add(urlprop, url)
+                    .add(descriptionprop, description)
+                    .add(contextprop, context)
                     .build().toString()
             )
         ).start();
-        final Request req = new ApacheRequest(container.home());
+        final Request entry = new ApacheRequest(container.home());
         final Statuses statuses = new RtStatuses(
-            req,
+            entry,
             new RtCommit(
-                req,
+                entry,
                 new MkGithub().randomRepo(),
                 "0abcd89jcabitest"
             )
         );
         try {
             statuses.create(
-                new RtStatus(
-                    Status.State.Failure, "http://example.com",
-                    "description", "ctx"
-                )
+                new Statuses.StatusCreate(Status.State.FAILURE)
+                    .withTargetUrl(Optional.of(url))
+                    .withDescription(description)
+                    .withContext(Optional.of(context))
             );
+            final MkQuery request = container.take();
             MatcherAssert.assertThat(
-                container.take().method(),
+                request.method(),
                 Matchers.equalTo(Request.POST)
             );
-            Matchers.equalToIgnoringCase(Status.State.Failure.name());
+            final JsonObject obj = Json.createReader(
+                new StringReader(request.body())
+            ).readObject();
+            MatcherAssert.assertThat(
+                obj.getString(stateprop),
+                Matchers.equalTo(Status.State.FAILURE.identifier())
+            );
+            MatcherAssert.assertThat(
+                obj.getString(contextprop),
+                Matchers.equalTo(context)
+            );
+            MatcherAssert.assertThat(
+                obj.getString(descriptionprop),
+                Matchers.equalTo(description)
+            );
+            MatcherAssert.assertThat(
+                obj.getString(urlprop),
+                Matchers.equalTo(url)
+            );
         } finally {
             container.stop();
         }
