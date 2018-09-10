dffb06ee2e8c2a32c6b35e3e5fa4ccdc8bf07b5b
George Aristy <george.aristy@securekey.com>
2018-09-10 17:14:58 -0400

    (#1430) MkHook.json

--- src/test/java/com/jcabi/github/mock/MkHookTest.java
+++ src/test/java/com/jcabi/github/mock/MkHookTest.java
@@ -33,16 +33,19 @@ import com.jcabi.aspects.Tv;
 import com.jcabi.github.Coordinates;
 import com.jcabi.github.Hook;
 import com.jcabi.github.Repo;
+import com.jcabi.xml.XML;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.junit.Before;
 import org.junit.Test;
+import org.xembly.Directives;
 
 /**
  * Tests for {@link MkHook}.
  * @author Paulo Lobo (pauloeduardolobo@gmail.com)
  * @version $Id$
  * @since 0.42
+ * @checkstyle MultipleStringLiteralsCheck (500 lines)
  */
 public final class MkHookTest {
 
@@ -50,6 +53,11 @@ public final class MkHookTest {
      * Hook to be tested.
      */
     private transient Hook hook;
+
+    /**
+     * XML encapsulated by the hook.
+     */
+    private transient XML xml;
 
     /**
      * {@link Repo} for testing.
@@ -68,12 +76,36 @@ public final class MkHookTest {
             "jcabi-github"
         );
         final MkStorage storage = new MkStorage.InFile();
+        storage.apply(
+            new Directives().xpath("/github")
+                .add("repos").add("repo").attr("coords", coordinates.toString())
+                .add("hooks")
+                .add("hook")
+                .add("id").set(String.valueOf(Tv.FIVE)).up()
+                .add("url").set("http://github.com/url").up()
+                .add("test_url").set("http://github.com/test_url").up()
+                .add("ping_url").set("http://github.com/ping_url").up()
+                .add("name").set("testname").up()
+                .add("events")
+                    .add("push").up()
+                    .add("pull_request").up()
+                    .up()
+                .add("active").set("true").up()
+                .add("config")
+                    .add("url").set("http://example.com/webhook").up()
+                    .add("content_type").set("json").up()
+                    .up()
+                .add("updated_at").set("2011-09-06T20:39:23Z").up()
+                .add("created_at").set("2011-09-06T17:26:27Z").up()
+        );
         this.hook =  new MkHook(
             storage,
             login,
             coordinates,
             Tv.FIVE
         );
+        this.xml = storage.xml()
+            .nodes(String.format("//hook[id=%s]", Tv.FIVE)).get(0);
         this.repo = new MkRepo(storage, login, coordinates);
     }
 
@@ -107,12 +139,14 @@ public final class MkHookTest {
      * Test if {@link MkHook} is being created with the correct id.
      * @throws Exception If something goes wrong
      */
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void createWithCorrectId() throws Exception {
         MatcherAssert.assertThat(
             "Hook json returned wrong id",
             this.hook.json().getInt("id"),
-            new IsEqual<Integer>(Tv.FIVE)
+            new IsEqual<Integer>(
+                Integer.parseInt(this.xml.xpath("id/text()").get(0))
+            )
         );
     }
 
@@ -120,13 +154,13 @@ public final class MkHookTest {
      * Test if {@link MkHook} is being created with the correct url.
      * @throws Exception If something goes wrong
      */
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void createWithCorrectUrl() throws Exception {
         MatcherAssert.assertThat(
             "Hook json returned wrong url",
             this.hook.json().getString("url"),
             new IsEqual<String>(
-                "https://api.github.com/repos/jcabi/jcabi-github/hooks/1"
+                this.xml.xpath("url/text()").get(0)
             )
         );
     }
@@ -135,13 +169,13 @@ public final class MkHookTest {
      * Test if {@link MkHook} is being created with the correct test url.
      * @throws Exception If something goes wrong
      */
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void createWithCorrectTestUrl() throws Exception {
         MatcherAssert.assertThat(
             "Hook json returned wrong test_url",
             this.hook.json().getString("test_url"),
             new IsEqual<String>(
-                "https://api.github.com/repos/jcabi/jcabi-github/hooks/1/test"
+                this.xml.xpath("test_url/text()").get(0)
             )
         );
     }
@@ -150,13 +184,13 @@ public final class MkHookTest {
      * Test if {@link MkHook} is being created with the correct ping url.
      * @throws Exception If something goes wrong
      */
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void createWithCorrectPingUrl() throws Exception {
         MatcherAssert.assertThat(
             "Hook json returned wrong ping_url",
             this.hook.json().getString("ping_url"),
             new IsEqual<String>(
-                "https://api.github.com/repos/jcabi/jcabi-github/hooks/1/pings"
+                this.xml.xpath("ping_url/text()").get(0)
             )
         );
     }
