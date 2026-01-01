158368adc156f22c1c4bfd96f70e42a33ad21c2e
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 12:53:39 +0300

    year up to 2026

--- src/it/basic-usage/src/main/java/com/jcabi/foo/Sample.java
+++ src/it/basic-usage/src/main/java/com/jcabi/foo/Sample.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2013-2025 Yegor Bugayenko
+ * SPDX-FileCopyrightText: Copyright (c) 2013-2026 Yegor Bugayenko
  * SPDX-License-Identifier: MIT
  */
 package com.jcabi.foo;
